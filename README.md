2055ab4da188eaab119a9f4c99d5ca857eb6c2ed
deadc0de6 <info@deadc0de.ch>
2020-05-31 11:33:53 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
+[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0)
+
 # mftail
-Colorized multiple file follower
+
+[mftail](https://github.com/deadc0de6/mftail) is multiple files follower (similar to `tail -f`) with color.
+
+# usage
+
+usage
+```bash
+mftail v0.1
+
+Usage: mftail <path>...
+  -v	Print version.
+  -version
+    	Print version.
+```
+
+# compile from source
+
+compile
+```bash
+go build
+mftail -help
+```
+
+# Contribution
+
+If you are having trouble using mftail, open an issue.
+
+If you want to contribute, feel free to do a PR.
+
+# License
+
+This project is licensed under the terms of the GPLv3 license.
---
+++ mftail.go
@@ -0,0 +1,237 @@
+// a multiple file follower tool similar to `tail -f`
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"golang.org/x/sys/unix"
+	"io"
+	"math/rand"
+	"os"
+	"path"
+	"time"
+	"unsafe"
+)
+
+const (
+	Version = "0.1"
+	Reset   = "\033[0m"
+	Color   = "\033[%dm"
+)
+
+var (
+	FileMatcher map[int]chan bool // wd -> channel
+	Colors                        = []int{31, 32, 33, 34, 35, 36, 37, 90, 91, 92, 93, 94, 95, 96}
+	EventCounts int               = 128
+)
+
+type fileEvent struct {
+	Fd    int // inotify file descriptor
+	Wd    int // watch descriptor
+	Path  string
+	Color string
+	Chan  chan bool
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+	FileMatcher = make(map[int]chan bool)
+}
+
+// check file exists and is not dir
+func isFile(path string) bool {
+	s, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !s.IsDir()
+}
+
+// watch a file for change
+func addWatch(fd int, path string) (int, error) {
+	var flag uint32 = unix.IN_MODIFY | unix.IN_ATTRIB | unix.IN_DELETE_SELF
+	wd, err := unix.InotifyAddWatch(fd, path, flag)
+	if err != nil {
+		return 0, err
+	}
+	return wd, nil
+}
+
+// unwatch a file
+func rmWatch(fd int, wd int) {
+	unix.InotifyRmWatch(fd, uint32(wd))
+}
+
+// follow file
+func follow(fe fileEvent) {
+	f, err := os.Open(fe.Path)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	bname := path.Base(fe.Path)
+
+	// go to end of file
+	f.Seek(0, os.SEEK_END)
+
+	r := bufio.NewReader(f)
+	for {
+		// wait on channel for trigger
+		b, ok := <-fe.Chan
+		if !ok {
+			// something's wrong
+			rmWatch(fe.Fd, fe.Wd)
+			return
+		}
+		if !b && !isFile(fe.Path) {
+			// file removed
+			rmWatch(fe.Fd, fe.Wd)
+			return
+		}
+
+		// read lines
+		for {
+			line, err := r.ReadBytes('\n')
+			if err != nil && err != io.EOF {
+				fmt.Fprintln(os.Stderr, err)
+				return
+			}
+			// print the line
+			if len(line) > 0 {
+				fmt.Printf("%s:%s%s%s", bname, fe.Color, string(line), Reset)
+			}
+			if err == io.EOF {
+				break
+			}
+		}
+	}
+}
+
+// read the events
+func readEvents(buf []byte, bytesRead int) {
+	var offset int
+	for offset <= bytesRead {
+		ev := *(*unix.InotifyEvent)(unsafe.Pointer(&buf[offset]))
+		m := ev.Mask
+		ch := FileMatcher[int(ev.Wd)]
+		if m&unix.IN_ATTRIB == unix.IN_ATTRIB {
+			// unlink
+			ch <- false
+		}
+		if m&unix.IN_DELETE_SELF == unix.IN_DELETE_SELF {
+			// file removed
+			ch <- false
+		}
+		if m&unix.IN_MODIFY == unix.IN_MODIFY {
+			// modified
+			ch <- true
+		}
+		offset += unix.SizeofInotifyEvent
+	}
+}
+
+// monitor for events
+func waitForNotif(fd int) error {
+	// buffer to store events
+	// field "Name" is not present/filled when monitoring files
+	// see https://linux.die.net/man/7/inotify
+	size := unix.SizeofInotifyEvent * EventCounts
+	var buf = make([]byte, size)
+	for {
+		// read the event
+		bytesRead, err := unix.Read(fd, buf[:])
+		if err != nil || bytesRead < unix.SizeofInotifyEvent {
+			continue
+		}
+		readEvents(buf[:], bytesRead)
+	}
+}
+
+// print version
+func version() {
+	name := path.Base(os.Args[0])
+	fmt.Fprintf(os.Stdout, "%s v%s\n", name, Version)
+}
+
+// print usage and exit
+func usage() {
+	version()
+	name := path.Base(os.Args[0])
+	fmt.Fprintf(os.Stdout, "\nUsage: %s <path>...\n", name)
+	flag.PrintDefaults()
+	os.Exit(1)
+}
+
+func main() {
+	// parse args
+	vshortArg := flag.Bool("v", false, "Print version.")
+	vlongArg := flag.Bool("version", false, "Print version.")
+	flag.Parse()
+
+	if *vshortArg || *vlongArg {
+		version()
+		os.Exit(0)
+	}
+
+	if len(flag.Args()) < 1 {
+		usage()
+	}
+
+	// randomize colors
+	rand.Shuffle(len(Colors), func(i, j int) { Colors[i], Colors[j] = Colors[j], Colors[i] })
+
+	// init inotify
+	fd, err := unix.InotifyInit()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	defer unix.Close(fd)
+
+	// parse file paths to monitor
+	var cnt int
+	for idx, path := range flag.Args() {
+		if !isFile(path) {
+			fmt.Fprintf(os.Stderr, "\"%s\" not found!\n", path)
+			continue
+		}
+
+		// add file to monitor
+		wd, err := addWatch(fd, path)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, fmt.Sprintf("\"%s\": %s", path, err.Error()))
+			continue
+		}
+		defer rmWatch(fd, wd)
+
+		// create a new channel for this file
+		ch := make(chan bool)
+		FileMatcher[wd] = ch
+
+		color := Colors[idx%len(Colors)]
+		fevent := fileEvent{
+			Fd:    fd,
+			Wd:    wd,
+			Path:  path,
+			Color: fmt.Sprintf(Color, color),
+			Chan:  ch,
+		}
+
+		// follow the file
+		go follow(fevent)
+		cnt++
+	}
+
+	if cnt < 1 {
+		// no file to follow
+		fmt.Fprintf(os.Stderr, "no file to follow\n")
+		os.Exit(1)
+	}
+
+	err = waitForNotif(fd)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
