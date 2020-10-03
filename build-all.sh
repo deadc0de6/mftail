be0d9d47fd032e6fb137c611f84de61a4d6631a0
deadc0de6 <info@deadc0de.ch>
2020-10-03 18:59:23 +0200

    add script to build artifacts

---
+++ build-all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+name="uad"
+goos="linux"
+for goarch in arm arm64 386 amd64; do
+  echo "building ${goos}-${goarch}"
+  env GOOS="${goos}" GOARCH="${goarch}" go build -v -o ${name}-${goos}-${goarch}
+done
+
