f184a30ed8586a24aba71745c26b011357bab221
deadc0de6 <info@deadc0de.ch>
2022-04-08 16:16:56 +0200

    fix tests

--- test.sh
+++ test.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+# deps
+go install golang.org/x/lint/golint@latest
+go install honnef.co/go/tools/cmd/staticcheck@latest
+
+# linting
 go fmt *.go
 golint -set_exit_status *.go
+staticcheck *.go
 go vet *.go
+
+# compiling
+make clean
+make
